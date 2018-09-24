dc41de30b49020d1ad6b4d5fec265dc8c6386d12
jeremy_glb <yxjeremy_glb@126.com>
2018-09-24 15:37:42 +0800

    Buid eureka server

---
+++ gc-test-platform-eureka-server/src/main/java/com/jeremy/gctestplatformeurekaserver/GcTestPlatformEurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.jeremy.gctestplatformeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class GcTestPlatformEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GcTestPlatformEurekaServerApplication.class, args);
+    }
+}
